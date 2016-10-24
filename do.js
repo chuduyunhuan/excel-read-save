1425af658671d9e68cdf29ba5b26a517bd444d90
devdongx <vitality-xing@163.com>
2016-10-24 17:29:37 +0800

    add

---
+++ do.js
@@ -0,0 +1,131 @@
+﻿var XLSX = require('xlsx');
+//配置文件
+var FILED_HIERARCHY = {
+    hieraarchyField: '物理热点(需与小区表中热点名称对应)',
+    disField: '物理热点（名称不可重复）',
+    hotSpotField: '层次关系',
+    detialedInfo: {
+        GSM: '2G小区',
+        TD: '3G小区',
+        LTE: '4G小区'
+    },    
+    commonField: {
+        name: '小区名称',
+        type: '',
+        lac: 'LAC',
+        ci: 'CI',
+        lng: '经度',
+        lat: '纬度',
+        direction: '方向角',
+        frequency: '载频数',
+        cellType: '基站类型（室内/宏站/应急车）'
+    },
+    '层次关系': {},
+    '2G小区': {},
+    '3G小区': {},
+    '4G小区': {}
+};
+//层次关系
+function readHieraarchy(path,col,rowBegin,rowEnd){
+    var hotSpotArr = [];
+    var workbook = XLSX.readFile(path);
+    var hieraarchySheet = workbook.Sheets[FILED_HIERARCHY.hotSpotField];
+    for(var i=rowBegin; i<rowEnd; ++i){
+        if(!hieraarchySheet[col+i]) continue;
+        var hotSpot = hieraarchySheet[col+i].v;
+        if(!hotSpot) continue;
+        hotSpotArr.push(hotSpot);
+    }
+    readDetailedInfo(workbook,hotSpotArr);
+}
+//获取详情数据
+function readDetailedInfo(workbook,arr){    
+    arr.map(function(val){   
+        var resultArr = [];     
+        for(var name in FILED_HIERARCHY.detialedInfo){
+            FILED_HIERARCHY.commonField.type = FILED_HIERARCHY.detialedInfo[name].substring(0,2);
+            var worksheet = workbook.Sheets[FILED_HIERARCHY.detialedInfo[name]];
+            //获取最终保存所需字段的列号
+            var colArr = []; 
+            //过滤字段提取,这儿可以优化
+            var colDisField = '';
+            var length = 0;
+            for (var z in worksheet) {
+                length++;
+                if(z[0] === '!') continue;
+                var temObj = FILED_HIERARCHY.commonField;
+                for(var field in temObj) {
+                    if(worksheet[z].v === FILED_HIERARCHY.disField){
+                        colDisField = z[0];
+                        continue;
+                    }
+                    if(worksheet[z].v !== temObj[field]) continue;
+                    colArr.push(z[0]);
+                }
+            }
+             //console.log(colArr);
+            // console.log(colDisField);
+            resultArr.push(extractFieldVal(colArr,colDisField,val,worksheet,length));
+        }
+        //console.log(resultArr);
+    });    
+}
+//根据行列号提取数据
+function extractFieldVal(colArr,colDisField,val,worksheet,length){
+    var result = [];
+    for(var i=2; i<length; ++i){
+        if(!worksheet[colDisField+i]) continue;
+        var hotSpot = worksheet[colDisField+i].v;
+        if(!hotSpot || hotSpot !== val) continue;
+        //构造所需字符串格式
+        var temStr = '';
+        for(var j=0,lenJ=colArr.length; j<lenJ; ++j){
+            if(!worksheet[colArr[j] + i]) continue;
+            var temVal = worksheet[colArr[j] + i].v;
+            temStr += temVal + '|';
+        }
+        result.push(temStr);
+    }
+    console.log(result);
+    // for (var z in worksheet) {
+    //     if(z[0] === '!') continue;
+    //     var temObj = FILED_HIERARCHY.commonField;
+    //     for(var field in temObj) {
+    //         if(worksheet[z].v === temObj[field]) continue;
+    //         if()
+    //     }
+    // }
+}
+function readExcel1(path){
+    var workbook = XLSX.readFile(path);
+    var sheet_name_list = workbook.SheetNames;
+    // console.log(sheet_name_list);
+    var hieraarchySheet = workbook.Sheets['层次关系'];
+    console.log(hieraarchySheet['D34'].v);
+    // for(var name in hieraarchySheet){
+    //     if(name[0] === '!') continue;
+    //     //找到物理热点对应的列编号
+    //     var hotSpotName = hieraarchySheet[name].v;
+    //     console.log(hotSpotName);
+    //     if(hotSpotName !== FILED_HIERARCHY.hieraarchyField) continue;
+    //     console.log(name[0]);
+    //     //console.log(JSON.stringify(hieraarchySheet[name].v));
+    // }
+    // console.log(hieraarchySheet);
+    // sheet_name_list.forEach(function(y) {
+    //     // console.log(y);
+    //   // iterate through sheets 
+    //   var worksheet = workbook.Sheets[y];
+    //   // for (z in worksheet) {
+    //   //   // all keys that do not begin with "!" correspond to cell addresses 
+    //   //   if(z[0] === '!') continue;
+    //   //   console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+    //   // }
+    // });
+}
+//保存
+function saveExcel(){
+
+}
+//readExcel('origin-files/test.xlsx');
+readHieraarchy('origin-files/test.xlsx','D',3,14);
---
+++ kerry.js
@@ -0,0 +1,213 @@
+﻿Array.prototype.unique = function()
+{
+    var n = {},r=[]; //n为hash表，r为临时数组
+    for(var i = 0; i < this.length; i++) //遍历当前数组
+    {
+        if (!n[this[i]]) //如果hash表中没有当前项
+        {
+            n[this[i]] = true; //存入hash表
+            r.push(this[i]); //把当前数组的当前项push到临时数组里面
+        }
+    }
+    return r;
+}
+//保存通用函数
+function datenum(v, date1904) {
+    if(date1904) v+=1462;
+    var epoch = Date.parse(v);
+    return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
+
+function sheet_from_array_of_arrays(data, opts) {
+    var ws = {};
+    console.log(data);
+    var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+    for(var R = 0; R != data.length; ++R) {
+        for(var C = 0; C != data[R].length; ++C) {
+            if(range.s.r > R) range.s.r = R;
+            if(range.s.c > C) range.s.c = C;
+            if(range.e.r < R) range.e.r = R;
+            if(range.e.c < C) range.e.c = C;
+            var cell = {v: data[R][C] };
+            if(cell.v == null) continue;
+            var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+            
+            if(typeof cell.v === 'number') cell.t = 'n';
+            else if(typeof cell.v === 'boolean') cell.t = 'b';
+            else if(cell.v instanceof Date) {
+                cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+                cell.v = datenum(cell.v);
+            }
+            else cell.t = 's';
+            
+            ws[cell_ref] = cell;
+        }
+    }
+    if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+    return ws;
+}
+//创建workbook
+function Workbook() {
+    if(!(this instanceof Workbook)) return new Workbook();
+    this.SheetNames = [];
+    this.Sheets = {};
+}
+
+var XLSX = require('xlsx');
+var FIELD = {
+    useSheet: 'Sheet1',
+    tableHead: [],
+    workbook: null
+};
+function readFile(path,col,fileName){
+    var workbook = XLSX.readFile(path);
+    FIELD.workbook = workbook;
+    var sheet_name_list = workbook.SheetNames;
+    var first_sheet_name = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[first_sheet_name];
+    var colorNumArr = [];
+    //计算总行数
+    var length = 0;
+    for(var z in worksheet){
+        length++;
+    }
+    //提取表头
+    var tableHead = [];
+    for(var z in worksheet){
+        if(z[0] === '!') continue;
+        var colNum = z.substring(1);
+        if(colNum > 1) continue;
+        tableHead.push(worksheet[z].v);
+    }
+    FIELD.tableHead.push(tableHead);
+
+    //提取主题内容
+    for(var i=2; i<=length; ++i){
+        if(!worksheet[col+i] || !worksheet[col+i].v) continue;
+        colorNumArr.push(worksheet[col+i].v);
+        //if(worksheet[z].v !== '色号') continue;
+    }
+    var result = colorNumArr.unique().sort();
+    //console.log(result);
+    onlySheet(result,worksheet,'B',length,fileName);
+}
+//多sheet
+function multiSheets(arr,worksheet,col,length){
+    var wb = new Workbook();
+    arr.map(function(val){
+        var sameColor = [];
+        //换行
+        var rowBreak = 0;
+        var temArr = [];
+        for(var z in worksheet){
+            if(z[0] === '!') continue;
+            var colNum = z.substring(1);
+            if(!worksheet[col+colNum] || !worksheet[col+colNum].v) continue;
+            if(worksheet[col+colNum].v !== val) continue;
+
+            if(rowBreak === 0){
+                rowBreak = colNum;
+                temArr.push(worksheet[z].v);
+            }else{
+                if(colNum !== rowBreak){
+                    rowBreak = colNum;
+                    sameColor.push(temArr);
+                    temArr = [];
+                    temArr.push(worksheet[z].v);
+                }else{
+                    temArr.push(worksheet[z].v);
+                }
+            }
+        }
+        //for in 循环后最后一次结果
+        sameColor.push(temArr);
+        setSheet(sameColor,val);
+        // saveExcel(sameColor,val,'kerry-processed/4776尺码明细.xls');
+    });  
+    function setSheet(arr,sheetName){
+        var data = FIELD.tableHead.concat(arr);
+        var ws = sheet_from_array_of_arrays(data);
+        var ws_name = sheetName;
+        wb.SheetNames.push(ws_name);
+        wb.Sheets[ws_name] = ws;
+    }  
+    XLSX.writeFile(wb, 'kerry-processed/4776尺码明细_处理.xls');
+}
+//单sheet
+function onlySheet(arr,worksheet,col,length,fileName){
+    var wb = new Workbook();
+    var sameColor = [];
+    var sumArr = [];
+    arr.map(function(val){
+        //换行
+        var rowBreak = 0;
+        var temArr = [];
+        for(var z in worksheet){
+            if(z[0] === '!') continue;
+            var colNum = z.substring(1);
+            if(!worksheet[col+colNum] || !worksheet[col+colNum].v) continue;
+            if(worksheet[col+colNum].v !== val) continue;
+
+            if(rowBreak === 0){
+                //换行
+                sameColor.push([null]);
+                rowBreak = colNum;
+                temArr.push(worksheet[z].v);
+            }else{
+                if(colNum !== rowBreak){
+                    rowBreak = colNum;
+                    sameColor.push(temArr);
+                    sumArr.push(temArr);
+                    temArr = [];
+                    temArr.push(worksheet[z].v);
+                }else{
+                    temArr.push(worksheet[z].v);
+                }
+            }
+        }
+        //for in 循环后最后一次结果
+        sameColor.push(temArr);
+        sumArr.push(temArr);
+        //求和
+        var target = addArr(sumArr);
+        sumArr.length = 0;
+        sameColor.push(['sum','--'].concat(target));
+    }); 
+    //console.log(sameColor);
+    saveExcel(sameColor,'处理','kerry-processed/' + '处理_' + fileName);
+}
+function addArr(arr){
+    var result = [];
+    if(arr.length === 0) return result;
+    for(var i=2; i<10; ++i){
+        var sum = 0;
+        arr.map(function(val){
+            sum += val[i];
+        });
+        result.push(sum);
+    }
+    return result;
+}
+function saveExcel(arr,sheetName,fileName){
+    var data = FIELD.tableHead.concat(arr);
+
+    var wb = new Workbook(), 
+        ws = sheet_from_array_of_arrays(data);
+        ws_name = sheetName;
+
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    XLSX.writeFile(wb, fileName);
+}
+function readDir(path){
+    var fs = require('fs');
+    fs.readdir(path,function(err,files){
+        files.map(function(fileName){
+            readFile(path + '/' + fileName, 'B', fileName);
+        });
+    });
+}
+//读取单个文件
+// readFile('kerry-origin/4776尺码明细.xls','B','处理_4776尺码明细.xls');
+//读取文件夹
+readDir('kerry-origin');
